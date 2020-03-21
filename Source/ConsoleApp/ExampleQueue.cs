632da79e3deff0bb3856eb4423868c4c8746e7e7
Walt Ritscher <waltritscher@outlook.com>
2020-03-21 00:07:57 -0700

    Add custom queue code

---
+++ Source/ConsoleApp/ExampleQueue.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp {
+	class ExampleQueue<T> {
+
+		private T[] _array;
+		private int _head;       // First valid element in the queue
+		private int _tail;       // Last valid element in the queue
+		private int _size;       // Number of elements.
+		private int _version;
+
+		private const int _MinimumGrow = 4;
+		private const int _ShrinkThreshold = 32;
+		private const int _GrowFactor = 200;  // double each time
+		private const int _DefaultCapacity = 4;
+		static T[] _emptyArray = new T[0];
+		public ExampleQueue() {
+			_array = _emptyArray;
+		}
+
+		public void Enqueue(T item) {
+			if (_size == _array.Length)
+			{
+				int newcapacity = (int)((long)_array.Length * (long)_GrowFactor / 100);
+				if (newcapacity < _array.Length + _MinimumGrow)
+				{
+					newcapacity = _array.Length + _MinimumGrow;
+				}
+				SetCapacity(newcapacity);
+			}
+
+			_array[_tail] = item;
+			_tail = (_tail + 1) % _array.Length;
+			_size++;
+			_version++;
+		}
+
+		public T Dequeue() {
+			if (_size == 0)
+				throw new Exception("No more items in the Queue");
+
+			T removed = _array[_head];
+			_array[_head] = default(T);
+			_head = (_head + 1) % _array.Length;
+			_size--;
+			_version++;
+			return removed;
+		}
+
+		// PRIVATE Grows or shrinks the buffer to hold capacity objects. Capacity
+		// must be >= _size.
+		private void SetCapacity(int capacity) {
+			T[] newarray = new T[capacity];
+			if (_size > 0)
+			{
+				if (_head < _tail)
+				{
+					Array.Copy(_array, _head, newarray, 0, _size);
+				}
+				else
+				{
+					Array.Copy(_array, _head, newarray, 0, _array.Length - _head);
+					Array.Copy(_array, 0, newarray, _array.Length - _head, _tail);
+				}
+			}
+
+			_array = newarray;
+			_head = 0;
+			_tail = (_size == capacity) ? 0 : _size;
+			_version++;
+		}
+	}
+}
--- Source/ConsoleApp/Program.cs
+++ Source/ConsoleApp/Program.cs
@@ -6,72 +6,65 @@ using System.Threading.Tasks;
 namespace ConsoleApp {
 
 	internal class Program {
-		private static long _total;
-		private static Queue<int> _intQueue;
 
-		private static void Main(string[] args) {
-			Console.WriteLine($"The correct total is 2,001,000");
-			try
-			{
+		static ExampleQueue<int> _exampleQueue;
+		private static void Main() {
 
-				Demo1();
 
-				#region Multi-thread Demos
-			//	Demo2();
-			//	Demo3(); 
-				#endregion
+			
+				try
+				{
+					Demo2();
+				} catch (Exception ex)
+				{
 
-			} catch (Exception ex)
-			{
-				Console.WriteLine($"Exception {ex}");
-			}
-			Console.ResetColor();
+					Console.ForegroundColor = ConsoleColor.Red;
+					Console.WriteLine($"Exception:{ex.Message}");
+					Console.ResetColor();
+				}
+		
 		}
 
 		private static void Demo1() {
-			SetupQueue();
-			CalculateTotalFromQueued();
-			Console.WriteLine($"Single Thread:: \r\n\tTotal: \t{_total:N0}");
+			_exampleQueue = new ExampleQueue<int>();
+			int result = 0;
+			SetupExampleQueue();
+
+
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
+		
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
+
+			Console.ResetColor();
+
 		}
-
-		private static void SetupQueue() {
-			IEnumerable<int> numbers = Enumerable.Range(1, 2000);
-			_intQueue = new Queue<int>(numbers);
-
-			_total = 0;
-		}
-
-		private static void CalculateTotalFromQueued() {
-			int temp;
-
-			while (_intQueue.TryDequeue(out temp))
-			{
-				//Console.Write($"{Thread.CurrentThread.ManagedThreadId}, ");
-				_total += temp;
-			}
-		}
-
-		#region Multi-thread Demos
 
 		private static void Demo2() {
-			SetupQueue();
-
-			Task task1 = Task.Run(() => CalculateTotalFromQueued());
-			Task task2 = Task.Run(() => CalculateTotalFromQueued());
-
+			int result = 0;
+			_exampleQueue = new ExampleQueue<int>();
+			Task task1 = Task.Run(() => SetupExampleQueue());
+			Task task2 = Task.Run(() => SetupExampleQueue());
 			Task.WaitAll(task1, task2);
-			Console.ForegroundColor = ConsoleColor.Yellow;
-			Console.WriteLine($"Task.Run:: \r\n\tTotal: \t{_total:N0}");
+
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
+
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
+			result = _exampleQueue.Dequeue();
+			Console.WriteLine($"Dequeue value:{result}");
 		}
+		private static void DoNothing() { }
+			private static void SetupExampleQueue() {
 
-		private static void Demo3() {
-			SetupQueue();
-
-
-			Parallel.Invoke(() => CalculateTotalFromQueued(), () => CalculateTotalFromQueued());
-			Console.ForegroundColor = ConsoleColor.Cyan;
-			Console.WriteLine($"Parallel.Invoke:: \r\n\tTotal: \t{_total:N0}");
-		} 
-		#endregion
+			for (int i = 0; i < 30; i++)
+			{
+				_exampleQueue.Enqueue(i);
+			}
+		}
 	}
 }
