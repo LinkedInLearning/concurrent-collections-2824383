dfa69dda1d1bf6666618b9fcfc3ae3b5405232af
walt-instructor <staffauthor@outlook.com>
2020-03-30 00:52:58 -0700

    Add dictionary example code

--- Source/ConsoleApp/Program.cs
+++ Source/ConsoleApp/Program.cs
@@ -1,20 +1,20 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Threading;
-using System.Threading.Tasks;
 
 namespace ConsoleApp
 {
 	internal class Program
 	{
-		private static Queue<Robot> _robots = new Queue<Robot>();
+		//private static Dictionary<int, Robot> _robots;
+		//private static ConcurrentDictionary<int, Robot> _robotsNew;
 
 		private static void Main(string[] args)
 		{
 			try
 			{
-				
-				Demo2();
+				Demo();
 			} catch (Exception ex)
 			{
 				Console.ForegroundColor = ConsoleColor.Red;
@@ -23,58 +23,73 @@ namespace ConsoleApp
 			}
 		}
 
-		
-		private static void Demo2()
+		private static void Demo()
 		{
-			_robots = new Queue<Robot>();
-			Robot robot;
-			Task task1 = Task.Run(() => SetupTeam1());
-			Task task2 = Task.Run(() => SetupTeam2());
-			Task.WaitAll(task1, task2);
+			var robots = new Dictionary<int, Robot>();
+			var _robotsNew = new ConcurrentDictionary<int, Robot>();
 
-			while (_robots.TryDequeue(out robot))
+			Robot robot;
+			robot = MakeRobot(teamName: "Star-chasers", ConsoleColor.DarkYellow);
+			robots.Add(robot.Id, robot);
+			robot = MakeRobot(teamName: "Star-chasers", ConsoleColor.DarkYellow);
+			robots.Add(robot.Id, robot);
+
+			robot = MakeRobot(teamName: "Deltron", ConsoleColor.Cyan);
+			robots.Add(robot.Id, robot);
+
+			robot = MakeRobot(teamName: "Dark Horizon", ConsoleColor.DarkMagenta);
+			robots.Add(robot.Id, robot);
+
+			WriteHeaderToConsole("List all items in dictionary");
+			foreach (var keyPair in robots)
 			{
-				Console.ForegroundColor = robot.TeamColor;
-				Console.WriteLine($"{robot.Id}: Team: {robot.Team}, {robot.Name}");
+				Console.ForegroundColor = keyPair.Value.TeamColor;
+				Console.WriteLine($"{keyPair.Key}: Team: {keyPair.Value.Name}, {keyPair.Value.Team}");
 			}
-				
-		
+			robots.Remove(1);
+
+			WriteHeaderToConsole("List after removing a robot");
+			foreach (var keyPair in robots)
+			{
+				Console.ForegroundColor = keyPair.Value.TeamColor;
+				Console.WriteLine($"{keyPair.Key}: Team: {keyPair.Value.Name}, {keyPair.Value.Team}");
+			}
+
 			Console.ResetColor();
+		}
+
+		private static void WriteHeaderToConsole(string headerText)
+		{
+			Console.ResetColor();
+			Console.WriteLine("-----------------------------");
+			Console.WriteLine(headerText);
 			Console.WriteLine("-----------------------------");
 		}
 
 		private static int _idCounter = 0;
-		private static object _lock = new object();
-		private static Mutex _mutex = new Mutex();
 
-		private static void MakeRobot(string teamName, ConsoleColor teamColor)
+		private static Robot MakeRobot(string teamName, ConsoleColor teamColor)
 		{
-
-		
 			Thread.Sleep(20);
 			_idCounter += 1;
 			var robot = new Robot { Id = _idCounter, Name = $"Robot {_idCounter}", Team = teamName, TeamColor = teamColor };
-			_robots.Enqueue(robot);
-
-		
+			return robot;
 		}
 
-		private static void SetupTeam1()
+		private static List<Robot> GetRobotTeam()
 		{
+			var temp = new List<Robot>();
 			for (int i = 0; i < 4; i++)
 			{
-
-				MakeRobot(teamName: "Starchasers", ConsoleColor.DarkYellow);
+				temp.Add(MakeRobot(teamName: "Star-chasers", ConsoleColor.DarkYellow));
 			}
-		}
 
-		private static void SetupTeam2()
-		{
 			for (int i = 0; i < 4; i++)
 			{
-
-				MakeRobot(teamName: "Deltron", ConsoleColor.Cyan);
+				temp.Add(MakeRobot(teamName: "Deltron", ConsoleColor.Cyan));
 			}
+
+			return null;
 		}
 	}
 }
