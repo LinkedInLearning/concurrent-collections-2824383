3f173ec072ba3043d3df58cd9e08b19a0fa2fd8e
walt-instructor <staffauthor@outlook.com>
2020-03-30 19:03:42 -0700

    Fix typo

--- Source/ConsoleApp/Program.cs
+++ Source/ConsoleApp/Program.cs
@@ -6,6 +6,8 @@ namespace ConsoleApp
 {
 	internal class Program
 	{
+		//private static Dictionary<int, Robot> _robots;
+		//private static ConcurrentDictionary<int, Robot> _robotsNew;
 
 		private static void Main(string[] args)
 		{
