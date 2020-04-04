e512ac84bdbeb929cb38e0d19fee775b2e985fe5
walt-instructor <staffauthor@outlook.com>
2020-04-03 22:19:07 -0700

    update example code

--- Source/ConsoleApp/Program.cs
+++ Source/ConsoleApp/Program.cs
@@ -35,34 +35,42 @@ namespace ConsoleApp
 			// IReadOnlyDictionary<TKey, TValue>, ICollection
 
 			var robotGems = new ConcurrentDictionary<string, int>();
-			
+
 			robotGems.TryAdd(key: "Robot1", value: 10);
 			robotGems.TryAdd(key: "Robot2", value: 20);
 			robotGems.TryAdd(key: "Robot3", value: 30);
 			robotGems.TryAdd(key: "Robot4", value: 40);
 
-			CreateReport(robotGems, 25);
-
 			//var collection = robotGems as ICollection<KeyValuePair<string, int>>;
-
+			CreateReport(robotGems);
+			RemoveItemsBelowThreshold(robotGems, 25);
 			Console.ResetColor();
 		}
 
 		private static Random _ran = new Random();
 
-		private static void CreateReport(ICollection<KeyValuePair<string, int>> candidates, int threshold)
+		private static void CreateReport(ICollection<KeyValuePair<string, int>> candidates)
 		{
 			// legacy service that produces report from an ICollection
 			foreach (var item in candidates)
 			{
 				Console.WriteLine($"{item.Key}:  GemCount: {item.Value}");
+			}
+		}
 
+		private static void RemoveItemsBelowThreshold(ICollection<KeyValuePair<string, int>> candidates, int threshold)
+		{
+			// legacy service that alters ICollection values
+			foreach (var item in candidates)
+			{
 				// .Add, .Remove are not thread-safe
 
 				if (item.Value < threshold)
 				{
 					candidates.Remove(item);
 				}
+
+				Console.WriteLine($"{item.Key}:  GemCount: {item.Value}");
 			}
 		}
 
