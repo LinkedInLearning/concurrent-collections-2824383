96ff7e041af52cd6175e985bbec1e3698580d2c7
walt-instructor <staffauthor@outlook.com>
2020-04-09 22:59:24 -0700

    Fix typo

--- Source/ConsoleApp/Program.cs
+++ Source/ConsoleApp/Program.cs
@@ -42,7 +42,7 @@ namespace ConsoleApp
 			// but it is more complex and you must provide one or more delegates
 			// that add or update the values in the ConcurrentDictionary
 			
-			//if (robotGems.TryUpdate("robot2", 22, 20))
+			if (robotGems.ContainsKey("robot2"))
 			//{
 			//	// update item value in dictionary.
 			//}
